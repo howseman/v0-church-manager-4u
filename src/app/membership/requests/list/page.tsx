e8cda15f199e4f545969f4181f70ec5946c10f6a
Howseman <howsmayer@hotmail.com>
2025-02-15 13:11:39 +0100

    Add basic layout and minimal page components

---
+++ src/app/membership/requests/list/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MembershipRequestsListPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Membership Requests List</h1>
+      <p>This is the membership requests list page.</p>
+    </div>
+  );
+};
+
+export default MembershipRequestsListPage;
